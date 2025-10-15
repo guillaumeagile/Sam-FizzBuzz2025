d7c4ea40e430fdfc245a6be3569696e4b8a9222a
guillaume prof <guillaume.saintetienne@iter.org>
2025-10-15 18:42:21 +0200

    Remove CUPID-Learning-Path.md and improve BaseRuleTests with clarity on `CompareTo` behavior and null handling conventions.

--- FizzBuzz.Tests/BaseRuleTests.cs
+++ FizzBuzz.Tests/BaseRuleTests.cs
@@ -1,78 +1,98 @@
 using FizzBuzz.Engine;
 using FluentAssertions;
 
-namespace FizzBuzz.Tests
+namespace FizzBuzz.Tests;
+
+[TestFixture]
+public class BaseRuleTests
 {
-    [TestFixture]
-    public class BaseRuleTests
+    // Concrete implementation of BaseRule for testing purposes
+    private class TestRule : BaseRule
     {
-        // Concrete implementation of BaseRule for testing purposes
-        private class TestRule : BaseRule
+        public TestRule(int priority = 1)
         {
-            public TestRule(int priority = 1)
-            {
-                base.Priority = priority;
-            }
-
-            public override string Evaluate(int number)
-            {
-                return number.ToString();
-            }
+            Priority = priority;
         }
 
-        [Test]
-        public void CompareTo_WithNullArgument_ReturnsOne()
+        public override string Evaluate(int number)
         {
-            // Arrange
-            var rule = new TestRule();
-
-            // Act
-            var result = rule.CompareTo(null);
-
-            // Assert
-            result.Should().Be(1);
+            return number.ToString();
         }
+    }
 
-        [Test]
-        public void CompareTo_WithSamePriority_ReturnsZero()
-        {
-            // Arrange
-            var rule1 = new TestRule(priority: 5);
-            var rule2 = new TestRule(priority: 5);
 
-            // Act
-            var result = rule1.CompareTo(rule2);
+    [Test]
+    public void CompareTo_WithSamePriority_ReturnsZero()
+    {
+        // Arrange
+        var rule1 = new TestRule(5);
+        var rule2 = new TestRule(5);
 
-            // Assert
-            result.Should().Be(0);
-        }
+        // Act
+        var result = rule1.CompareTo(rule2);
 
-        [Test]
-        public void CompareTo_WithLowerPriorityRule_ReturnsNegative()
-        {
-            // Arrange
-            var rule1 = new TestRule(priority: 3);
-            var rule2 = new TestRule(priority: 5);
+        // Assert
+        result.Should().Be(0);
+    }
 
-            // Act
-            var result = rule1.CompareTo(rule2);
+    [Test]
+    public void CompareTo_WithLowerPriorityRule_ReturnsNegative()
+    {
+        // Arrange
+        var rule1 = new TestRule(3);
+        var rule2 = new TestRule(5);
 
-            // Assert
-            result.Should().BeLessThan(0);
-        }
+        // Act
+        var result = rule1.CompareTo(rule2);
 
-        [Test]
-        public void CompareTo_WithHigherPriorityRule_ReturnsPositive()
-        {
-            // Arrange
-            var rule1 = new TestRule(priority: 7);
-            var rule2 = new TestRule(priority: 3);
+        // Assert
+        result.Should().BeLessThan(0);
+    }
 
-            // Act
-            var result = rule1.CompareTo(rule2);
+    [Test]
+    public void CompareTo_WithHigherPriorityRule_ReturnsPositive()
+    {
+        // Arrange
+        var rule1 = new TestRule(7);
+        var rule2 = new TestRule(3);
 
-            // Assert
-            result.Should().BeGreaterThan(0);
-        }
+        // Act
+        var result = rule1.CompareTo(rule2);
+
+        // Assert
+        result.Should().BeGreaterThan(0);
+    }
+
+
+    [Test] // ask yourself why this test is failing
+    public void CompareTo_WithNullArgument_ReturnsOne()
+    {
+        // Arrange
+        var rule = new TestRule();
+
+        // Act
+        var result = rule.CompareTo(null);
+        //shall we accept null or not ?
+        //we must be explicit about it, if we accept null, we must return 1
+
+        // Assert
+        result.Should().Be(1);
+
+        // WE NEED A DISCUSSION HERE TO START THE WORKSHOP
+
+        // do we have to raise an exception ?
+        //
+        // short answer : NO, because we are comparing two objects, and C# standards say
+        // Standard .NET Behavior: According to .NET conventions, any object is considered greater than null.
+        // When comparing with null, the method should return 1 (or any positive number).
+        // with NULLABLES we can be explicit about it, if we not raise an exception, we must return 1
+
+
+        // INDEEP: Idiomatic of C# allows us to accept null, at the level of the CompareTo method
+        // but our Product Owner said that it should be impossible to insert a null rule in the list
+        //
+
+
+        // NOW WE ARE READY TO MOVE TO CUPID !!!!!!
     }
 }
