f6263b945f0c6e743efb5f74848005770c45f668
guillaume prof <guillaume.saintetienne@iter.org>
2025-10-26 18:13:45 +0100

    à vous de coder

--- FizzBuzz.Engine/Rules/Concretes/ExactMatchRule.cs
+++ FizzBuzz.Engine/Rules/Concretes/ExactMatchRule.cs
@@ -5,13 +5,10 @@ namespace FizzBuzz.Engine.Rules.Concretes
 {
     /// <summary>
     /// A rule that outputs text for an exact number match and stops further processing
-    /// Using record for value-based equality (functional programming)
     /// </summary>
     public record ExactMatchRule(int TargetNumber, string Output) : RuleBase
     {
         public override RuleResult Evaluate(int number) => 
-            number == TargetNumber 
-                ? RuleResult.StopWith(Output) 
-                : RuleResult.Empty;
+         throw new NotImplementedException();
     }
 }
--- FizzBuzz.Tests/RulesTests.cs
+++ FizzBuzz.Tests/RulesTests.cs
@@ -1,6 +1,7 @@
 using FizzBuzz.Engine.Rules.Concretes;
 using FizzBuzz.Engine.Rules.Result;
 using FluentAssertions;
+using Shouldly;
 
 namespace FizzBuzz.Tests;
 
@@ -31,21 +32,39 @@ public class RulesTests
     [Test]
     public void TheRuleThatDividesBy3PrintsFizz()
     {
-        var rule = new DefaultRule();
-        var result = rule.Evaluate(99);
+        var rule = new DivisibilityRule(33, "Fizz");
+        var result = rule.Evaluate(9);
 
         result.Should().BeOfType<RuleResult.Continue>();
         result.Output.Should().Be("Fizz");
     }
 
     [Test]
-    public void TheRuleThatStopsBecauseItIsTheAnswerToEverything()
+    public void TheRuleThatDividesBy3PrintsNothingIfTheNumberIsNotDivisibleBy3()
     {
-        var rule = new DefaultRule();
-        var result = rule.Evaluate(0);
+        var rule = new DivisibilityRule(3, "Fizz");
+        var result = rule.Evaluate(5);
 
         result.Should().BeOfType<RuleResult.Continue>();
-        result.Output.Should().Be("the answer to everything");
+        result.Output.Should().BeEmpty( );
+    }
+
+    [Test]
+    [TestCase(3, "", typeof(RuleResult.Continue) )]
+    [TestCase(5, "", typeof(RuleResult.Continue) )]
+    [TestCase(41, "", typeof(RuleResult.Continue))]
+    [TestCase(42, "the answer to everything", typeof(RuleResult.Final))]
+    [TestCase(43, "", typeof(RuleResult.Continue))]
+    public void TheRuleThatStopsBecauseItIsTheAnswerToEverything(int number,
+        string expectedOutput,
+        Type expectedOutputType)
+    {
+        var rule = new ExactMatchRule(41, "the answer to everything");
+        var result = rule.Evaluate(number);
+
+    //   result.Should().BeOfType<RuleResult.Continue>();
+        result.ShouldBeOfType(expectedOutputType);
+        result.Output.Should().Be(expectedOutput);
     }
 
 }
